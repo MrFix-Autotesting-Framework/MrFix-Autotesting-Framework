caef0b55d8c14833c2fb9e1c3824cf9aa2152787
Mr. Fix <121102996+MrFix-Autotesting-Framework@users.noreply.github.com>
2024-11-13 19:10:40 +0300

    For MrFix 7.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mrfix",
-    version="7.0.0",
+    version="7.0.1",
     description="The Mr Fix module is designed to create autotests written in Python for testing UI, API, PostgreSQL, Security and Perfomance Testing.",
     packages=["mrfix"],
     install_requires=[
